923c79b8fb6c2e93c76d5b0ae06b3610bacc7702
corvo <148205799+corvo-99@users.noreply.github.com>
2024-02-22 11:57:59 +0100

    implemented the 3 different scrollable views

--- DateCasino/DateCasino/ContentView.swift
+++ DateCasino/DateCasino/ContentView.swift
@@ -11,14 +11,18 @@ import SwiftUI
 struct ContentView: View {
     
     @State var spinn1 : Bool = false
-    
+    @State var spinn2 : Bool = false
+    @State var spinn3 : Bool = false
     var body: some View {
         HStack {
             Scrollable(spinn1: $spinn1)
-            Scrollable(spinn1: $spinn1)
-            Scrollable(spinn1: $spinn1)
+            Scrollable2(spinn2: $spinn2)
+            Scrollable3(spinn3: $spinn3)
             Button("lever"){
                 spinn1.toggle()
+                spinn2.toggle()
+                spinn3.toggle()
+                
                 print("\(spinn1)")
             }
         }
@@ -28,5 +32,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView(spinn1: false )
+    ContentView(spinn1: false, spinn2: false, spinn3: false )
 }
--- DateCasino/DateCasino/Scrollable3.swift
+++ DateCasino/DateCasino/Scrollable3.swift
@@ -1,8 +1,115 @@
 //
-//  Scrollable3.swift
+//  Scrollable.swift
 //  DateCasino
 //
-//  Created by Giuseppe Olivari on 22/02/24.
+//  Created by Giuseppe Olivari on 21/02/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct Scrollable3: View {
+    
+    @State private var finalText1 = "Not get"
+    @State private var scrollText = false
+    @State private var boh = 0
+    @State private var touch = true
+    @State private var blockSpinn3 = false
+    @Binding var spinn3 : Bool
+   
+    var attr1 : [String] = ["first", "second", "third","fourth","fifth","sixth"]
+
+    let arr1 = (0..<1000000).map( {_ in Int.random(in: 0...5)} )
+   
+    
+    
+    var body: some View {
+        
+    
+                    VStack{
+                        
+                        ScrollViewReader { scrollView in
+                            ScrollView(showsIndicators: false){
+                                LazyVStack(){
+                                    ForEach(0..<1000000) { index in
+                                        
+                                        Text(attr1[arr1[index]])
+                                            .font(.title2)
+                                            .id(index)
+                                        
+                                        
+                                    }
+                                }
+                            }
+                            .foregroundColor(.green)
+                            .scrollDisabled(true)
+                            .frame(height: 130)
+                            
+                            
+                            .onChange(of: spinn3) { newValue in
+                                            
+                                            if newValue {
+                                                
+                                                    if !blockSpinn3{
+                                                        animateWithTimer(proxy: scrollView, boh: self.boh,touch: self.touch)
+                                                        self.spinn3.toggle()
+                                                    
+                                                    }
+                                            }
+                                        }
+                            
+                            
+                        }
+                        Button("Block"){
+                            blockSpinn3.toggle()
+                        }
+                        
+                    }
+        
+        
+                }
+    
+    func animateWithTimer(proxy: ScrollViewProxy, boh: Int, touch : Bool) {
+        let count: Int = 10
+        let duration: Double = 1.0
+        let timeInterval: Double = (duration / Double(count))
+        var counter = self.boh
+        
+//        print("\(counter)")
+        let random = Int.random(in:(counter + 50)...(counter+100))
+//        let random = Int.random(in: 0...50)
+        let timer = Timer.scheduledTimer(withTimeInterval: timeInterval, repeats: true) { (timer) in
+            withAnimation(.default) {
+                
+                proxy.scrollTo(counter, anchor: .center)
+                
+                //print("fin:\(fin)")
+                finalText1 = attr1[arr1[counter]]
+                self.boh = counter
+                
+                
+             
+            }
+            if counter < random {
+                counter+=1
+                
+            }else{
+                
+                DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(4), execute: {
+                    timer.invalidate()
+                    //self.spinn1.toggle()
+                })
+                
+            }
+        }
+        timer.fire()
+      
+    }
+    
+    
+    
+    
+            }
+                    #Preview {
+                        Scrollable3(spinn3: .constant(false))
+                    }
+                
