50748ac0daadfaa1cc6fd4958533cde5c15f83b5
giuseppeolivari <golivari1@gmail.com>
2024-02-23 10:06:35 +0100

    added new final page

---
+++ DateCasino/DateCasino/FinalAnimation.swift
@@ -0,0 +1,18 @@
+//
+//  FinalAnimation.swift
+//  DateCasino
+//
+//  Created by Giuseppe Olivari on 23/02/24.
+//
+
+import SwiftUI
+
+struct FinalAnimation: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FinalAnimation()
+}
