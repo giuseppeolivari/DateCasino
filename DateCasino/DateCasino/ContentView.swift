6d7efe4974d22fda50e98cc9431739dae0941c6c
corvo <148205799+corvo-99@users.noreply.github.com>
2024-02-27 21:42:46 +0100

    cleaned ContentView

--- DateCasino/DateCasino/ContentView.swift
+++ DateCasino/DateCasino/ContentView.swift
@@ -9,7 +9,6 @@ import SwiftUI
 
 
 struct ContentView: View {
-    
     @State var spinn1 : Bool = false
     @State var spinn2 : Bool = false
     @State var spinn3 : Bool = false
@@ -17,61 +16,36 @@ struct ContentView: View {
     @State var finalText2 = "b"
     @State var finalText3 = "c"
     @State var isPresented : Bool = false
-    
-    
     //@State  var blockSpinn = false
     //@State  var blockSpinn2 = false
     // @State  var blockSpinn3 = false
-    
     var body: some View {
-        
-        ZStack{
-            //            Image("background")
-            //                .resizable()
-            //
-            //                .ignoresSafeArea()
-            VStack{
-                HStack{
-                    ZStack {
-                        Group {
-                            VStack {
-                                Scrollable(finalText0: $finalText0/*, blockSpinn: $blockSpinn*/, spinn1: $spinn1)
-                                Scrollable2(finalText2: $finalText2/*, blockSpinn2: $blockSpinn2*/, spinn2: $spinn2)
-                                    .padding(.top)
-                                Scrollable3(finalText3: $finalText3/*, blockSpinn3: $blockSpinn3*/, spinn3: $spinn3)
-                                    .padding(.top)
-                            }
-                            .frame(width: 250, height: 350, alignment: .top)
-                            
-                        }
-                        ZStack {
-                            Image("background")
-                                .resizable()
-                                .ignoresSafeArea()
-                            HStack {
-                                Spacer()
-                                
-                                Button(action: { }) {
-                                    AnimatedLeverView(spinn1: $spinn1, spinn2: $spinn2, spinn3: $spinn3, action: toggleSpins)
-                                    
-                                }
-                                
-                            }
-                            Button(action: {
-                               
-                            }) {
-                                GetFinalView(isPresented: $isPresented, finalText0: $finalText0, finalText2: $finalText2, finalText3: $finalText3)
-                                
-                            }
-                        }
+        ZStack {
+            VStack {
+                Scrollable(finalText0: $finalText0/*, blockSpinn: $blockSpinn*/, spinn1: $spinn1)
+                Scrollable2(finalText2: $finalText2/*, blockSpinn2: $blockSpinn2*/, spinn2: $spinn2)
+                    .padding(.top)
+                Scrollable3(finalText3: $finalText3/*, blockSpinn3: $blockSpinn3*/, spinn3: $spinn3)
+                    .padding(.top)
+            }
+            .frame(width: 250, height: 350, alignment: .top)
+            ZStack {
+                Image("background")
+                    .resizable()
+                    .ignoresSafeArea()
+                HStack {
+                    Spacer()
+                    Button(action: { }) {
+                        AnimatedLeverView(spinn1: $spinn1, spinn2: $spinn2, spinn3: $spinn3, action: toggleSpins)
                     }
+                }
+                Button(action: { }) {
+                    GetFinalView(isPresented: $isPresented, finalText0: $finalText0, finalText2: $finalText2, finalText3: $finalText3)
                 }
             }
         }
-        
     }
     func toggleSpins() {
-        
         spinn1.toggle()
         spinn2.toggle()
         spinn3.toggle()
