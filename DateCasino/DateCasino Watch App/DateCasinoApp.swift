f5d777e3647689591d26877f90ad8bb4498354ec
Dom-mennillo2 <dmennillo23@fed.idserve.net>
2024-02-29 12:03:32 +0100

    LOCALIZATION

--- DateCasino/DateCasino Watch App/DateCasinoApp.swift
+++ DateCasino/DateCasino Watch App/DateCasinoApp.swift
@@ -6,12 +6,34 @@
 //
 
 import SwiftUI
+import TipKit
 
+struct CrownTip: Tip{
+    var title : Text {
+        Text("Use the crown to spin")
+            
+            
+            
+        
+    }
+    
+        
+}
 @main
 struct DateCasino_Watch_AppApp: App {
+    init() {
+        try? Tips.configure()
+            
+      }
     var body: some Scene {
         WindowGroup {
             ScrollableView1()
         }
+    }
+}
+
+extension String {
+    func localized() -> String {
+        return NSLocalizedString(self, tableName: "Localizable", bundle: .main, value: self, comment: self)
     }
 }
